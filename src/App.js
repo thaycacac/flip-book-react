dc3627dc121201c536a8bd0f3c742e1e068cc57c
thaycacac <thaycacac@gmail.com>
2020-02-24 22:07:33 +0700

    update: core

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Flipbook from './components/Flipbook'
+import './App.css'
 
 function App() {
+
+  const initPages = [
+    null,
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/6.jpg'
+  ]
+
+  const initPagesHiRes = [
+      null,
+      'images-large/1.jpg',
+      'images-large/2.jpg',
+      'images-large/3.jpg',
+      'images-large/4.jpg',
+      'images-large/5.jpg',
+      'images-large/6.jpg'
+  ]
+
+  const [hasMouse, setHasMouse] = useState(true)
+  const [pages, setPages] = useState(initPages)
+  const [pagesHires, setPagesHires] = useState(initPagesHiRes)
+
   return (
-    <div className="App">
-        <Flipbook />
+    <div id="app"
+      className={hasMouse ? 'has-mouse' : ''}
+      onTouchStart={() => setHasMouse(false)}
+    >
+        <Flipbook
+          class="flipbook"
+          pages={pages}
+          pagesHiRes={pagesHires}
+         />
     </div>
   );
 }
--- src/components/Flipbook.js
+++ src/components/Flipbook.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Flipbook = () => {
+
+    const [hasTouchEvents, setHasTouchEvents] = useState(false)
+    const [touchStartX, setTouchStartX] = useState(null)
+    const [touchStartY, setTouchStartY] = useState(null)
+
+    const onTouchStart = (ev) => {
+        console.log('onTouchStart', ev);
+        setHasTouchEvents(true)
+        swipeStart(ev.changedTouches[0])
+    }
+
+    const swipeStart = (touch) => {
+        setTouchStartX(touch.pageX)
+        setTouchStartY(touch.pageY)
+    }
+
     return (
-        <h1>hello</h1>
+        <div
+            onTouchStart={(ev) => onTouchStart(ev)}
+        >
+            fsdafsdfsdfdsfdsfsdfsdf
+        </div>
     )
 }
-    
+
 export default Flipbook
